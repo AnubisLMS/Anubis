7f2c71d5a8ab5650735b9464d4d47b0ac5767f95
NamanChimnani <48537699+NamanChimnani@users.noreply.github.com>
2020-01-10 00:21:41 +0530

    Created webhook test setup (#3)
    * Created webhook test setup
    
    * Created webhook test setup
    
    Co-authored-by: big_J <36013983+jmc1283@users.noreply.github.com>

--- api/src/routes/public.py
+++ api/src/routes/public.py
@@ -1,24 +1,26 @@
 from flask import request, redirect, url_for, flash, render_template, Blueprint
 from json import dumps
 
+from ..config import Config
 from ..app import db
 from ..models import Submissions, Results, Events
 from ..utils import enqueue_webhook_job, log_event, get_request_ip
 
 public = Blueprint('public', __name__, url_prefix='/public')
 
-
-@public.route('/webhook', methods=['GET', 'POST'])
+# dont think we need GET here
+@public.route('/webhook', methods=['POST'])
 @log_event('JOB-REQUEST', get_request_ip)
 def webhook():
     """
     This route should be hit by the github when a push happens.
     We should take the the github repo url and enqueue it as a job.
     """
-    if request.method == 'POST':
-        pass
-    repo_url = 'test'
-    enqueue_webhook_job(repo_url)
+
+    if request.headers['Content-Type'] == 'application/json':
+        received_data = request.json
+        repo_url = Config.REPO_SKELETAL + received_data["sender"]["login"]
+        enqueue_webhook_job(repo_url)
 
     return dumps({
         'success': True
