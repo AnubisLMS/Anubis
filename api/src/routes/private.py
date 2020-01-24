49c0878737a050f869cde3a31a97e143001dccab
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-01-23 21:45:31 -0500

    implement the error handling enpoint fully

--- api/src/routes/private.py
+++ api/src/routes/private.py
@@ -4,7 +4,7 @@ from json import dumps
 
 from ..app import db
 from ..models import Submissions, Reports, Events
-from ..utils import log_event
+from ..utils import log_event, send_noreply_email
 private = Blueprint('private', __name__, url_prefix='/private')
 
 
@@ -16,13 +16,54 @@ def notify_student(submission):
 
 @private.route('/')
 def index():
-    return 'super secret'
+    return 'super duper secret'
 
 
 @private.route('/report-error', methods=['POST'])
-@log_event('ERROR-REPORT', lambda: 'error report from worker submitted')
+@log_event('ERROR-REPORT', lambda: 'error report from worker ' + dumps(request.json))
 def handle_report_error():
-    print(dumps(request.json, indent=2), flush=True)
+    """
+    If at any point, a worker in the rq cluster encounters an error
+    with grading an assignment, the worker should report to this endpoint.
+    This does not necessarily mean that something is broken in the cluster.
+    errors could include:
+
+    - Clone errors
+    - Build errors
+    - Various docker errors
+
+    The body of the post request should fit the shape:
+
+    request.json = {
+      netid         : str - netid of student
+      assignment    : str - name of assignment
+      submission_id : int - database id for submission that was processed
+      error         : str - description of error encountered
+    }
+
+    When this route is hit, we should mearly report the error back to the student
+    through a noreply email.
+    """
+    req = request.json
+    print(dumps(body, indent=2), flush=True)
+
+    msg = """
+    There was an error in grading your recent OS3224 submission.
+
+    netid: {netid}
+    assignment: {assignment}
+    error: {error}
+    """.format(
+        netid=req['netid'],
+        assignment=req['assignment'],
+        error=req['error'],
+    )
+
+    send_noreply_email(
+        msg,
+        'OS3224 - Autograder Error', # email subject
+        req['netid'] + '@nyu.edu',   # recipient
+    )
     return {'success':True}
 
 
