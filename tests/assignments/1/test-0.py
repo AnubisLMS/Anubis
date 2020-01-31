ebb9ba70e14afe753c23b7575f372240b03c012c
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-01-31 00:49:46 -0500

    start to implement assignment 1 tests

--- tests/assignments/1/test-0.py
+++ tests/assignments/1/test-0.py
@@ -1,11 +1,24 @@
 from utils import save_results
 import os
+import subprocess
+import time
 
-os.system('timeout 5 qemu-system-i386 -serial mon:stdio -drive file=./submission/xv6.img,media=disk,index=0,format=raw -drive file=./submission/fs.img,media=disk,index=1,format=raw -smp 1 -m 512 -display none -nographic')
+qemu_cmd = 'timeout 5 qemu-system-i386 -serial mon:stdio -drive file=./submission/xv6.img,media=disk,index=0,format=raw -drive file=./submission/fs.img,media=disk,index=1,format=raw -smp 1 -m 512 -display none -nographic'
 
+def test_1():
+    qemu = subprocess.Popen(qemu_cmd + ' <<< "\nuniq README.md\n"', stdout=subprocess.PIPE, shell=True)
+    expected = subprocess.check_output('uniq ./submission/README.md', shell=True)
+    qemu.wait()
+    stdout, err = qemu.communicate()
+    stdout = stdout.decode().split('\n')
+    lines = stdout[stdout.index('init: starting sh'):]
+    
 
-save_results(
-    'test-0',
-    None,
-    True
-)
+try:
+    pass
+except:
+    save_results(
+        'test-0',
+        ['nah'],
+        False
+    )
