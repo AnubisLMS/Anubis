2ce6c3350c8481f4d97e51dadb92a29e2a022de9
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-02-29 21:00:28 -0500

    fix another goof

--- tests/assignments/os3224-assignment-2/test-1.py
+++ tests/assignments/os3224-assignment-2/test-1.py
@@ -23,32 +23,33 @@ print(
     ' '.join(str(i) for i in l2),
 )
 
-try:
-    prog = subprocess.Popen(
-        ['./submission/linkedList'] + [str(i) for i in l1] + [','] + [str(i) for i in l2],
-        stdout=subprocess.PIPE,
-        stderr=None
-    )
-    prog.wait()
-    stdout, err = prog.communicate()
-    stdout = stdout.decode().split('\n')
 
-    print('output lines parsed:', stdout)
-
-    ans = [line.startswith(expected) for line in stdout]
-    if any(ans):
-        print('expected result was found')
-        save_results(
-            'test-1',
-            ['expected result was found'],
-            True
+def test():
+    try:
+        prog = subprocess.Popen(
+            ['./submission/linkedList'] + [str(i) for i in l1] + [','] + [str(i) for i in l2],
+            stdout=subprocess.PIPE,
+            stderr=None
         )
-        exit(0)
-except:
-    pass
+        prog.wait()
+        stdout, err = prog.communicate()
+        stdout = stdout.decode().split('\n')
+        print('output lines parsed:', stdout)
+        ans = [line.startswith(expected) for line in stdout]
+        if any(ans):
+            print('expected result was found')
+            return save_results(
+                'test-1',
+                ['expected result was found'],
+                True
+            )
+    except:
+        pass
 
-save_results(
-    'test-1',
-    ['expected result was not found'],
-    False
-)
+    save_results(
+        'test-1',
+        ['expected result was not found'],
+        False
+    )
+
+test()
