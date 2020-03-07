82a98e2a36ba5a6500cf2a9f221acc914736a2b0
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-03-07 12:47:11 -0500

    add timezone awareness

--- tests/test.sh
+++ tests/test.sh
@@ -15,17 +15,19 @@ if ! docker-compose ps api | grep Up &> /dev/null; then
 fi
 cd tests
 
-echo 'bringing up services... (this will take a while)'
-cd ..
-set -x
-docker-compose pull --parallel &> /dev/null &
-make debug  &> /dev/null
-make cli &> /dev/null
-set +x
-cd tests
+if [ "$1" != "--skip" ]; then
+    echo 'bringing up services... (this will take a while)'
+    cd ..
+    set -x
+    docker-compose pull --parallel &> /dev/null &
+    make debug  &> /dev/null
+    make cli &> /dev/null
+    set +x
+    cd tests
+    echo 'giving anubis a hot second to start...'
+    sleep 5
+fi
 
-echo 'giving anubis a hot second to start...'
-sleep 5
 
 echo 'test uploading student data'
 anubis -d student ./students.json | jq
@@ -44,6 +46,6 @@ echo 'test assignment 2'
 echo 'giving assignment a hot second to process'
 sleep 5
 
-anubis -d stats | jq
+anubis -d assignment ls | jq
 anubis -d stats os3224-assignment-2 | jq
 anubis -d stats os3224-assignment-2 jmc1283 | jq
