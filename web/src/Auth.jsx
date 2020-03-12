2a7281bc146ced091b542bd2428d815d4c5b1abb
John McCann Cunniff Jr <36013983+jmc1283@users.noreply.github.com>
2020-03-11 23:49:32 -0400

    add more verbose errors to website (#34)

--- web/src/Auth.jsx
+++ web/src/Auth.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,82 +7,30 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import {makeStyles} from "@material-ui/core/styles";
-import {green} from "@material-ui/core/colors";
+import {Redirect} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    maxWidth: 936,
-    margin: theme.spacing(2),
-    padding: theme.spacing(2),
-    overflow: 'hidden',
-  },
-  searchBar: {
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-  },
-  searchInput: {
-    fontSize: theme.typography.fontSize,
-  },
-  title: {
-    fontSize: 14,
-  },
-  block: {
-    display: 'block',
-  },
-  addUser: {
-    marginRight: theme.spacing(1),
-  },
-  contentWrapper: {
-    margin: '40px 16px',
-  },
-  card: {
-    minWidth: 275,
-  },
-  list: {
-    width: '100%',
-  },
   dialog: {
     margin: theme.spacing(2),
     padding: theme.spacing(2)
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-  progress: {
-    padding: theme.spacing(1)
-  },
-  fabProgress: {
-    color: green[500],
-    position: 'absolute',
-    top: -6,
-    left: -6,
-    zIndex: 1,
-  },
-  buttonProgress: {
-    color: green[500],
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -12,
-    marginLeft: -12,
-  },
-  wrapper: {
-    margin: theme.spacing(1),
-    position: 'relative',
   },
 }));
 
 export default function Auth(props) {
   const classes = useStyles();
+  const [netid, setNetid] = useState('');
+  const [redirect, setRedirect] = useState(false);
   const {
     open,
     onClose,
-    verify,
     commit,
-    setNetid
   } = props;
+
+  if (redirect) {
+    return (
+      <Redirect to={`/view/${commit}/${netid}`} />
+    );
+  }
 
   return (
     <Dialog open={open} onClose={onClose} className={classes.dialog}>
@@ -100,12 +48,12 @@ export default function Auth(props) {
           margin={'dense'}
           variant={'outlined'}
           label={'netid'}
-          onChange={e => setNetid(e.target.value)}
-          onKeyPress={e => e.key === 'Enter' ? verify() : null}
+          onChange={e => setNetid(e.target.value.trim())}
+          onKeyPress={e => e.key === 'Enter' ? setRedirect(true) : null}
         />
       </DialogContent>
       <DialogActions>
-        <Button variant="contained" color="primary" onClick={verify}>
+        <Button variant="contained" color="primary" onClick={() => setRedirect(true)}>
           Verify
         </Button>
       </DialogActions>
