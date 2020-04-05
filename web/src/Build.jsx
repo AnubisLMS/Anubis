d0c13d1961e4a9c181ea187e5fcc26df0644a98f
John McCann Cunniff Jr <36013983+jmc1283@users.noreply.github.com>
2020-04-05 16:51:37 -0400

    implement simplified interface (#46)

--- web/src/Build.jsx
+++ web/src/Build.jsx
@@ -1,107 +1,66 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import {useSnackbar} from 'notistack';
-import {green} from '@material-ui/core/colors';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from "@material-ui/core/Tooltip";
+import CheckIcon from "@material-ui/icons/Check";
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    maxWidth: 936,
-    margin: theme.spacing(2),
-    padding: theme.spacing(2),
-    overflow: 'hidden',
-  },
-  searchBar: {
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-  },
-  searchInput: {
-    fontSize: theme.typography.fontSize,
-  },
-  title: {
-    fontSize: 14,
-  },
-  block: {
-    display: 'block',
-  },
-  addUser: {
-    marginRight: theme.spacing(1),
-  },
-  contentWrapper: {
-    margin: '40px 16px',
-  },
-  card: {
-    minWidth: 275,
-  },
-  list: {
+  root: {
     width: '100%',
   },
-  dialog: {
-    margin: theme.spacing(2),
-    padding: theme.spacing(2)
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-  progress: {
-    padding: theme.spacing(1)
-  },
-  fabProgress: {
-    color: green[500],
-    position: 'absolute',
-    top: -6,
-    left: -6,
-    zIndex: 1,
-  },
-  buttonProgress: {
-    color: green[500],
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -12,
-    marginLeft: -12,
-  },
-  wrapper: {
-    margin: theme.spacing(1),
-    position: 'relative',
-  },
+  title: {
+    padding: theme.spacing(0.5),
+  }
 }));
 
 export default function Build({data}) {
   const classes = useStyles();
-  const {enqueueSnackbar} = useSnackbar();
 
   if (!data) {
     return (
       <div/>
     );
-  } else {
-    // enqueueSnackbar('Build succeeded', {variant: 'success', preventDuplicate: true});
   }
 
   return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Build Logs
+    <ExpansionPanel className={classes.root}>
+      <ExpansionPanelSummary
+        expandIcon={<ExpandMoreIcon/>}
+        aria-label="Expand"
+        aria-controls={`build`}
+        id={`build-panel`}
+      >
+        <IconButton edge={'start'}>
+          <Tooltip title={'Build Succeeded'}>
+            <CheckIcon
+              color={'primary'}
+              fontSize={'small'}
+            />
+          </Tooltip>
+        </IconButton>
+        <Typography className={classes.title} variant={'subtitle1'}>
+          build
         </Typography>
-        {data ? data.trim().split('\n').map((line, index) => (
-          <Typography
-            className={classes.pos}
-            // component="pre"
-            variant={"body1"}
-            color={"textSecondary"}
-            width={100}
-            key={`line-${index}`}
-          >
-            {line}
-          </Typography>
-        )) : null}
-      </CardContent>
-    </Card>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        <div>
+          {data ? data.trim().split('\n').map((line, index) => (
+            <Typography
+              variant={"body1"}
+              color={"textSecondary"}
+              width={100}
+              key={`line-${index}`}
+            >
+              {line}
+            </Typography>
+          )) : null}
+        </div>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
   )
 }
