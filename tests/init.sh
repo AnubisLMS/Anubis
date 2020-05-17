1cfa2121ed30cc8f1d6af78af5bf9fa0bfd9e079
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-05-17 19:05:58 -0400

    add final tests

--- tests/init.sh
+++ tests/init.sh
@@ -20,4 +20,5 @@ anubis -d assignment add os3224-assignment-1 '2020-03-07 23:55:00' '2020-03-08 2
 anubis -d assignment add os3224-assignment-2 '2020-03-07 23:55:00' '2020-03-08 23:55:00' | jq
 anubis -d assignment add os3224-assignment-3 '2020-04-05 23:55:00' '2020-04-06 23:55:00' | jq
 anubis -d assignment add midterm '2020-04-13 23:55:00' '2020-04-13 23:55:00' | jq
-anubis -d assignment add os3224-assignment-4 '2020-05-20 23:55:00' '2020-05-21 23:55:00' | jq
+anubis -d assignment add os3224-assignment-4 '2020-05-03 23:55:00' '2020-05-04 23:55:00' | jq
+anubis -d assignment add final '2020-05-20 23:55:00' '2020-05-20 23:55:00' | jq
