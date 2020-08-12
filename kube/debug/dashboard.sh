647f115c526156c6248b8bd04650f3ff9fb80e24
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-08-11 20:49:19 -0400

    ADD provisioning and debugging scripts

--- kube/debug/dashboard.sh
+++ kube/debug/dashboard.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
+
 echo https://127.0.0.1:8443/
+kubectl -n kubernetes-dashboard describe secret admin-user-token | grep ^token
 kubectl port-forward service/kubernetes-dashboard 8443:443 -n kubernetes-dashboard
