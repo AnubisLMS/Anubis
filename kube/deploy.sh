bfe5b185f9281ca3e0b288875bb503a8961a5d3a
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2020-09-08 16:51:02 -0400

    ADD helm chart

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
+set -e
 cd $(dirname $(realpath $0))
 
 
 kubectl config use-context space
 
+
 if ! kubectl get namespace | grep anubis &> /dev/null; then
     kubectl create namespace anubis
 fi
 
+
 if ! kubectl get secrets -n anubis | grep api &> /dev/null; then
+    read -s -p "Anubis DB Password: " DB_PASS
     kubectl create secret generic api \
-            --from-literal=database-uri=mysql+pymysql://anubis:anubis@mariadb.mariadb.svc.cluster.local/anubis \
+            --from-literal=database-uri=mysql+pymysql://anubis:${DB_PASS}@mariadb.mariadb.svc.cluster.local/anubis \
             --from-literal=secret-key=$(head -c10 /dev/urandom | openssl sha1 -hex | awk '{print $2}') \
             -n anubis
 fi
 
 
-
 pushd ..
 docker-compose build api
 docker-compose build --parallel web logstash
+if ! docker image ls | awk '{print $1}' | grep 'registry.osiris.services/anubis/api-dev' &>/dev/null; then
+   docker-compose build api-dev
+fi
 docker-compose push
 popd
 
+
 ../pipeline/build.sh --push
 
-docker push registry.osiris.services/anubis/assignment-base:ubuntu-16.04
-docker push registry.osiris.services/anubis/assignment/1:latest
 
-kubectl apply \
-        -f config/api.yml \
-        -f config/web-static.yml \
-        -f config/elk.yml \
-        -f config/redis.yml \
-        -f config/pipeline-api.yml \
-        -f config/rpc-workers.yml
+helm install anubis ./helm -n anubis
+
+# kubectl apply \
+#         -f config/api.yml \
+#         -f config/web-static.yml \
+#         -f config/elk.yml \
+#         -f config/redis.yml \
+#         -f config/pipeline-api.yml \
+#         -f config/rpc-workers.yml
 
 
 kubectl rollout restart deployments.apps/anubis-api -n anubis
-kubectl rollout restart deployments.apps/web -n anubis
+kubectl rollout restart deployments.apps/anubis-web -n anubis
 kubectl rollout restart deployments.apps/anubis-pipeline-api -n anubis
 kubectl rollout restart deployments.apps/anubis-rpc-workers  -n anubis
