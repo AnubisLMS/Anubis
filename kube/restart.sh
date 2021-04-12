d63a328a09d74ebe976bb844e9b2c36a7fbcbb68
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-04-12 15:11:46 -0400

    ADD rpc-regrade to restart script

--- kube/restart.sh
+++ kube/restart.sh
@@ -8,3 +8,4 @@ kubectl rollout restart deployments.apps/pipeline-api -n anubis
 kubectl rollout restart deployments.apps/theia-proxy  -n anubis
 kubectl rollout restart deployments.apps/rpc-default  -n anubis
 kubectl rollout restart deployments.apps/rpc-theia  -n anubis
+kubectl rollout restart deployments.apps/rpc-regrade  -n anubis
