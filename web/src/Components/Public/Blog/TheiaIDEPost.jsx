ca9f0af49c7de1906a67bb7a3aaf024da0c2936c
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-04-13 21:18:04 -0400

    CHG update theia post

--- web/src/Components/Public/Blog/TheiaIDEPost.jsx
+++ web/src/Components/Public/Blog/TheiaIDEPost.jsx
@@ -24,7 +24,11 @@ export default function TheiaIDEPost({classes, preview = false}) {
 
           <Typography className={classes.typography}>
             One of the more exciting new features of Anubis is that of the cloud ide. Leveraging some magic
-            that Kubernetes pods give us, we can provide a fully isolated IDE environment for all
+            that
+            <Link href={'https://kubernetes.io/docs/concepts/workloads/pods/'} target={'_blank'}>
+              {' '}Kubernetes pods{' '}
+            </Link>
+            give us, we can provide a fully isolated IDE environment for all
             ~130 students in Intro to OS concurrently.
           </Typography>
 
@@ -54,7 +58,15 @@ export default function TheiaIDEPost({classes, preview = false}) {
               </Typography>
               <Typography className={classes.typography}>
                 In this new optimized build of theia, we cut out all the extra stuff that exists in the normal theia-cpp
-                image, and add in the new
+                image, and add the extra packages that we need for xv6. For example, we can install qemu and libc6-i386
+                so that we can build and run xv6.
+              </Typography>
+              <Typography className={classes.typography}>
+                The optimization does not end there. Anubis supports using specific IDE builds for specific
+                assignments. What this means is that we can have specific IDEs for specific assignments. For some
+                classes, the dependencies that are necessary can be annoying to install on different individuals
+                machines. With Anubis, you just need to make it work in the cloud IDE, then a stable and consistent
+                environment is available to all students.
               </Typography>
 
               {/* Theia IDE Frontend */}
@@ -103,28 +115,39 @@ export default function TheiaIDEPost({classes, preview = false}) {
                 Then when we need to initialize a client session, we use this saved ClusterIP to forward requests (both
                 http and websockets) to the pod.
               </Typography>
+              <BlogImg
+                src={'https://anubis.osiris.services/api/public/static/0de69b0786007e60'}
+                alt={'theia-init-1'}
+              />
               <Typography className={classes.typography}>
                 These pods are temporary. When the student is finished working (or after a timeout) we reclaim the
                 resources by deleting the containers and data. Because of this, we needed some form of autosave. Saving
-                is pushing to github. The issue we need to contend with is how do we have automatic commits and pushes
-                to github without exposing a password or api token to the users. We handle this by having a second
-                container whose only role is committing and pushing to github. The credentials live in that container,
-                completely separate and inaccessible to the user who may be working in the other theia server container.
-                These two containers are connected by a shared longhorn volume. This volume is relatively small
-                (~50MiB). With this setup, we have autosave running in the background while being completely hidden from
-                the user.
+                is pushing to github. The issue we need to contend with is how to have automatic commits and pushes
+                to github without exposing a password or api token to the users.
+              </Typography>
+              <Typography className={classes.typography}>
+                The solution to this dilemma is to have a second sidecar container whose only role is committing and
+                pushing to github. The credentials live in that container, completely separate and inaccessible to the
+                user who may be working in the other theia server container. These two containers are connected by a
+                shared <Link href={'https://rancher.com/products/longhorn/'} target={'_blank'}>{' '}longhorn{' '}</Link>
+                volume. This volume is relatively small (~50MiB). With this setup, we have autosave
+                running in the background while staying completely hidden from the user.
               </Typography>
               <BlogImg
                 src={'https://anubis.osiris.services/api/public/static/9e023fa2714d8cdd'}
                 alt={'theia-pod.mmd.png'}
               />
+
+              <Typography className={classes.subheader}>
+                Scalability of Cloud IDE
+              </Typography>
               <Typography className={classes.typography}>
                 With these lightweight containerized theia servers, we are able to support significantly more concurrent
-                users than if we had elected to implement a cloud vm solution. Because with containers, we do not need
-                to virtualize hardware, the resources on the system for each user is significantly less. Given the
-                resources that we have in the Space Cluster, we would be able to have maybe 20-30 concurrent users using
-                cloud virtual machines. With our containerized theia, we can handle all ~130 students at the same time
-                with room to breath.
+                users than if we had elected to implement a cloud vm solution. Because these are containers and we do
+                not need to virtualize hardware, the resources on the system for each user is significantly less. Given
+                the resources that we have in the Space Cluster, we would be able to have maybe 20-30 concurrent users
+                using cloud virtual machines. With our containerized theia, we can handle all ~130 students at the same
+                time with room to breath.
               </Typography>
 
               {/* Reclaiming Theia Resources */}
@@ -132,12 +155,12 @@ export default function TheiaIDEPost({classes, preview = false}) {
                 Reclaiming Theia Resources
               </Typography>
               <Typography className={classes.typography}>
-                The theia sessions are often forgotten about. Students often create an IDE server, work on it for a bit,
-                then forget about it. Due to this, we have a time to live of 6 hours for each session. A cronjob runs
-                every 5 minutes to look for and schedule delete for stale resources. We do provide a button in the
-                anubis panel as a way for students to manually schedule their session for deletion. Even when we ask
-                students to click this button when they are done, some still do not. In the following graph we can see
-                an interesting experiment in student behavior. It shows a cumulative graph showing the duration of a
+                The theia sessions are often forgotten about. Students will create an IDE server, work on it for a bit,
+                then forget manually close it. Due to this, we have a time to live of 6 hours for each session. A
+                cronjob runs every 5 minutes to look for and schedule delete for stale resources. We do provide a button
+                in the anubis panel as a way for students to manually schedule their session for deletion. Even when we
+                ask students to click this button when they are done, some still do not. In the following graph we can
+                see an interesting experiment in student behavior. It shows a cumulative graph showing the duration of a
                 theia session for the final exam. Just about 40% of sessions hit the 6-hour timeout.
               </Typography>
               <BlogImg
