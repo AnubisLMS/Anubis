fce689f86ee631b1fa0193cad833c4f61e0aefef
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-04-22 22:47:57 -0400

    CHG make copyright open in new tab

--- web/src/Components/Copyright.jsx
+++ web/src/Components/Copyright.jsx
@@ -6,7 +6,7 @@ export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link href={'/api/public/memes/'}>
+      <Link href={'/api/public/memes/'} target={'_blank'}>
         Memes
       </Link>{' '}
       {new Date().getFullYear()}
