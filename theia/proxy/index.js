934afd94f2c8a157be41d1df4665d2ceaca354bf
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-05-01 20:08:01 -0400

    CHG simplify theia proxy LRU cache

--- theia/proxy/index.js
+++ theia/proxy/index.js
@@ -8,16 +8,7 @@ const LRU = require("lru-cache");
 
 const SECRET_KEY = process.env.SECRET_KEY ?? 'DEBUG';
 
-const cache = new LRU({
-  max: 100,
-  length: function (n, key) {
-    return n * 2 + key.length
-  },
-  dispose: function (key, n) {
-    n.close()
-  },
-  maxAge: 1000 * 60 * 60,
-})
+const cache = new LRU(100);
 
 const knex = Knex({
   client: "mysql",
