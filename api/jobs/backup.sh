d718a87183bb21976c9fbde7fe8a0b58d36f2133
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-05-30 23:58:30 -0400

    FIX issue with backup yaml

--- api/jobs/backup.sh
+++ api/jobs/backup.sh
@@ -4,6 +4,7 @@ export BACKUP_FILE="anubis-$(date +%F_%H%M%S).sql.gz" \
        HOME=/home/anubis
 
 cat /home/anubis/.ssh/config
+echo
 
 set -ex
 
@@ -16,7 +17,7 @@ mysqldump \
     anubis \
     | gzip - > /tmp/${BACKUP_FILE}
 
-if [ -n "${BACKUP_SSH_DOMAIN}" ]; then
+if [ -n "${SFTP_LOCATION}" ]; then
     echo 'scping file to remote'
     echo "put /tmp/${BACKUP_FILE} ${SFTP_LOCATION}" | sftp ${SFTP_USER}@${SFTP_HOST}
 fi
