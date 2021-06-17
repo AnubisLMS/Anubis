fa0a3dc450e0decdb656958e89d9db36b0cd883e
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-06-17 10:44:22 -0400

    CHG replace logstash with filbeat

--- api/anubis/utils/services/logger.py
+++ api/anubis/utils/services/logger.py
@@ -1,16 +1,14 @@
 import logging
 
-import logstash
-
 from anubis.config import config
 
 
 def _get_logger(logger_name):
     """
     For any of the anubis services that use the API, the
-    logger name should be specified so that all the logs
-    that are thrown at logstash, then elasticsearch
-    can be tracked back to a specific service.
+    logger name should be specified. In prod, container
+    logs will be captured by filebeat and pushed into
+    elasticsearch in the elastic namespace.
 
     :param logger_name:
     :return:
@@ -24,11 +22,8 @@ def _get_logger(logger_name):
     # Set the log level to debug if in debug mode
     if is_debug():
         _logger.setLevel(logging.DEBUG)
-
-    # If the disable ELK option is not enabled, then
-    # add the logstash udp logging layer.
-    if not config.DISABLE_ELK:
-        _logger.addHandler(logstash.LogstashHandler("logstash", 5000))
+    else:
+        _logger.setLevel(logging.INFO)
 
     return _logger
 
