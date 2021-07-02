85fb6fe7bf2c6a2830cf0c88cd07eb1e4d1ac3cb
nysteo <teonys@nyu.edu>
2021-07-02 14:40:01 -0400

    ADD: Colors

--- about/tailwind.config.js
+++ about/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/***.{js, jsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        primary: '#FF9900',
+        secondary: '#171B21',
+        tertiary: 'A5A5A5',
+      }
+    },
   },
   variants: {
     extend: {},
