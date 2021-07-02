fe813ab98ba19783b4e2611e495a7b7535ab1d09
nysteo <teonys@nyu.edu>
2021-07-02 16:35:44 -0400

    CHG: fix logo

--- about/src/components/atoms/logo.js
+++ about/src/components/atoms/logo.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import {StaticImage} from "gatsby-plugin-image";
+import {Link} from 'gatsby';
 
 
 const Logo = () => (
-  <StaticImage className = 'h-auto w-40' src={'../../images/logo.png'} quality={100} alt={'Anubis Logo'}/>
+  <Link to = '/'>
+    <StaticImage className = 'h-auto w-40 hover:opacity-50 cursor-pointer' src={'../../images/logo.png'} quality={100} alt={'Anubis Logo'}/>
+  </Link>
 );
 
 export default Logo;
