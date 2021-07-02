d7522062c1b7cebc493f4a59513fdd5dc3e09843
nysteo <teonys@nyu.edu>
2021-07-02 14:34:46 -0400

    ADD: tailwind css

--- about/gatsby-config.js
+++ about/gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Anubis About`,
+    description: `Anubis is a distributed LMS (learning management system), that is specifically designed for automating CS courses.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,12 +24,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-postcss`,
   ],
 }
