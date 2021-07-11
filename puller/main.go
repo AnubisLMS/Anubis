3854d7fdb59c8d2f18ca991caf462955baa99bfd
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-07-11 13:41:10 -0400

    CHG handle docker config json file when loading token

--- puller/main.go
+++ puller/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"io"
-	"io/ioutil"
-	"time"
 	"context"
 	"encoding/base64"
 	"encoding/json"
-	
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
-
 
 func indexOf(s string, r rune) int {
 	for i, sc := range s {
@@ -42,16 +41,23 @@ func parseToken(token string) string {
 	return base64.URLEncoding.EncodeToString(encodedJSON)
 }
 
-
 func getToken() string {
 	data, err := ioutil.ReadFile("/anubis/.dockerconfigjson")
-    if err != nil {
+	if err != nil {
 		panic(err)
-    }
+	}
 
-	return string(data)
+	var dockerconfig map[string](map[string](map[string]string))
+	var token string
+
+	json.Unmarshal(data, &dockerconfig)
+
+	for _, value := range dockerconfig["auths"] {
+		token = value["auth"]
+	}
+
+	return token
 }
-
 
 func pullImage(image string) {
 	authStr := parseToken(getToken())
@@ -62,6 +68,7 @@ func pullImage(image string) {
 	out, err := cli.ImagePull(context.Background(), image, types.ImagePullOptions{RegistryAuth: authStr})
 	if err != nil {
 		fmt.Printf("Failed to pull image: %s %s\n", err, image)
+		return
 	}
 
 	defer out.Close()
