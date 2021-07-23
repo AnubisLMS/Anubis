c92ca57fda7774c8017960a1cac058bbe428c454
Maxwell Yang <31149339+AlpacaMax@users.noreply.github.com>
2021-07-24 04:07:26 +0800

    v3.1.29 CHG rename professor column in Course to professor_display_name (#189)
    * CHG rename professor column in Course to professor_display_name
    
    * FIX trivial build failures
    
    * ADD trailing comma
    
    * CHG admin course access to professor_display_name

---
+++ api/migrations/versions/49d73a536492_chg_rename_professor_to_professor_for_.py
@@ -0,0 +1,23 @@
+"""chg_rename_professor_to_professor_for_course
+
+Revision ID: 49d73a536492
+Revises: 65be3bf01dae
+Create Date: 2021-07-18 12:57:52.105841
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '49d73a536492'
+down_revision = '65be3bf01dae'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('course', 'professor', new_column_name='professor_display_name', type_=sa.TEXT())
+
+def downgrade():
+    op.alter_column('course', 'professor_display_name', new_column_name='professor', type_=sa.TEXT())
