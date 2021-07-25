051888a88e37c67e5e772245604c79ceb4db8764
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-07-24 23:29:14 -0400

    v3.1.31 CLN summer cleaning 3 (#193)
    * FIX new deployment names in k8s scripts
    
    * CHG regenerate design doc

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -12,20 +12,9 @@ BUILD=(
     theia-sidecar
 )
 
-#if ! docker image ls | awk '{print $1}' | grep -w '^registry.digitalocean.com/anubis/theia-admin$' &>/dev/null; then
-#    EXTRA_BUILD="theia-admin"
-#fi
-#if ! docker image ls | awk '{print $1}' | grep -w '^registry.digitalocean.com/anubis/theia-xv6$' &>/dev/null; then
-#    EXTRA_BUILD="${EXTRA_BUILD} theia-xv6"
-#fi
-
 pushd ..
 docker-compose build --parallel --pull ${BUILD} ${EXTRA_BUILD}
 docker-compose push ${BUILD} ${EXTRA_BUILD}
 popd
 
-if ! helm list -n anubis | awk '{print $1}' | grep anubis &> /dev/null; then
-    exec helm install anubis ./chart -n anubis $@
-else
-    exec helm upgrade anubis ./chart -n anubis $@
-fi
+exec helm upgrade --install anubis ./chart -n anubis $@
