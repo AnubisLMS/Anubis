5072bb6a24696d7c188875fd4bcabed98c614cb4
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-08-09 14:51:10 -0400

    CHG adjust theia-init to handle long term persisten volume theia directory structure

--- theia/init/entrypoint.sh
+++ theia/init/entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+if [ ! "${GIT_REPO}" ]; then
+    echo "GIT_REPO is empty, exiting"
+    exit 0
+fi
 
 if [ -n "${GIT_CRED}" ]; then
     echo "${GIT_CRED}" > /root/.git-credentials
@@ -11,7 +16,17 @@ fi
 set -x
 
 # Clone
-git clone ${GIT_REPO} /out
+cd /out
+git clone ${GIT_REPO}
+
+set +x
+
+# Pull any and all repos
+for i in $(find . -maxdepth 2 -name '.git' -type d); do
+    pushd $(dirname $i)
+    git pull --no-verify
+    popd
+done
 
 # Fix permissions
 chown -R 1001:1001 /out
