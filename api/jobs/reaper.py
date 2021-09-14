2ea3eef0677546c916140eff2fc2675080643945
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-09-14 10:58:14 -0400

    FIX imports in reaper

--- api/jobs/reaper.py
+++ api/jobs/reaper.py
@@ -5,8 +5,8 @@ from typing import List
 
 from anubis.models import db, Submission, Assignment, Course
 from anubis.utils.data import with_context
-from anubis.utils.lms.autograde import bulk_autograde
-from anubis.utils.lms.submissions import init_submission
+from anubis.lms.autograde import bulk_autograde
+from anubis.lms.submissions import init_submission
 from anubis.utils.github.fix import fix_github_missing_submissions, fix_github_broken_repos
 from anubis.utils.logging import logger
 from anubis.utils.rpc import enqueue_ide_reap_stale, enqueue_autograde_pipeline
