51be66f1593f0bc38f70716903d580819114d9d1
as14692 <as14692@nyu.edu>
2021-09-27 15:46:12 -0400

    CHG Assignment Card

--- web/src/Components/Public/Assignments/AssignmentCardV2/AssignmentCardV2.jsx
+++ web/src/Components/Public/Assignments/AssignmentCardV2/AssignmentCardV2.jsx
@@ -6,22 +6,58 @@ import CardActions from '@material-ui/core/CardActions';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import clsx from 'clsx';
 
 import {useStyles} from './AssignmentCardV2.styles';
 
-const AssignmentCard = ({
+
+const AssignmentCardV2 = ({
   id,
   name,
   due_date,
   course,
 }) => {
   const classes = useStyles();
-
+  const due_days = Math.round((new Date(due_date) - new Date())/(1000*3600*24));
   return (
-    <div>
 
-    </div>
+    <Box className={classes.asignmentContainer}>
+      <Box
+        className={
+          clsx(classes.dueBadge,
+            due_days > 7 ? classes.greenBadge : due_days <=5 && due_days > 3 ? classes.orangeBadge : classes.redBadge)
+        }
+      >
+        <Typography className={classes.dueDate}>
+          {`${due_days} ${due_days <=1 ? 'day' : 'days'} remaining`}
+        </Typography>
+      </Box>
+      <Typography className={classes.assignmentName}>{name}</Typography>
+      <Typography className={classes.courseName}>{course.name}</Typography>
+      <Box className={classes.courseActionsContainer}>
+        <Button
+          onClick = {() => {
+            history.push(`/courses/assignments?courseId=${id}`);
+          }}
+          className={classes.openCourseButton}
+        >
+            Open Assignment
+        </Button>
+        <Button
+          onClick = {() => {
+            history.push(`/courses/assignments?courseId=${id}`);
+          }}
+          className={classes.viewRepoButon}
+        >
+          View Repo
+        </Button>
+      </Box>
+
+    </Box>
+
   );
 };
 
-export default AssignmentCard;
+
+export default AssignmentCardV2;
--- web/src/Components/Public/Assignments/AssignmentCardV2/AssignmentCardV2.styles.jsx
+++ web/src/Components/Public/Assignments/AssignmentCardV2/AssignmentCardV2.styles.jsx
@@ -1,7 +1,68 @@
 import {makeStyles} from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-  courseCardContainer: {
-    minWidth: '300px',
+  asignmentContainer: {
+    color: theme.palette.white,
+    flexGrow: 1,
+    minWidth: 275,
+    maxWidth: 280,
+    backgroundColor: theme.palette.dark.blue['200'],
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: '20px',
+    borderRadius: '10px',
+    margin: theme.spacing(5),
+  },
+  dueBadge: {
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    position: 'relative',
+    padding: `${theme.spacing(0.5)}px ${theme.spacing(1.5)}px ${theme.spacing(0.5)}px ${theme.spacing(0.5)}px`,
+    borderRadius: '3px',
+    top: -theme.spacing(5),
+    right: -theme.spacing(16),
+    flexGrow: 1,
+    minWidth: 100,
+    maxWidth: 150,
+    display: 'flex',
+
+  },
+  dueDate: {
+    color: 'black',
+  },
+  greenBadge: {
+    backgroundColor: theme.palette.color.green,
+  },
+  redBadge: {
+    backgroundColor: theme.palette.color.red,
+  },
+  orangeBadge: {
+    backgroundColor: theme.palette.color.orange,
+  },
+  assignmentName: {
+    fontSize: '20px',
+  },
+  courseName: {
+    fontSize: '16px',
+    color: theme.palette.gray['200'],
+  },
+  courseActionsContainer: {
+    marginTop: '20px',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  openCourseButton: {
+    fontSize: '14px',
+    borderRadius: '3px',
+    backgroundColor: theme.palette.primary.main,
+    padding: '5px 10px 5px',
+  },
+  viewRepoButon: {
+    fontSize: '.8rem',
+    backgroundColor: theme.palette.dark.blue['0'],
+    marginLeft: '10px;',
+    color: theme.palette.gray['200'],
   },
 }));
