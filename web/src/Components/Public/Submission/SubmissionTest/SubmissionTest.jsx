8ba23a0ccb78a4dd817e47e2652d73cc16f2fbcf
busisatya@gmail.com <busisatya@gmail.com>
2021-10-10 12:29:00 -0400

    CHG: Removed dummy data, used actual data

--- web/src/Components/Public/Submission/SubmissionTest/SubmissionTest.jsx
+++ web/src/Components/Public/Submission/SubmissionTest/SubmissionTest.jsx
@@ -4,44 +4,46 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import clsx from 'clsx';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-const SubmissionTest = ({
-}) =>{
+
+
+export default function SubmissionTest({tests}) {
   const classes = useStyles();
-  const submission = {
-    name: 'Test1',
-    success: true,
-  };
+
+  if (!tests) {
+    return null;
+  }
   return (
-    <Box
-      className={classes.root}
-    >
-      <Box
-        className={classes.title}
-      >
-        <Typography
-          className={classes.iconWrapper}
+    <React.Fragment>
+      {tests.map((test, index) => (
+        <Box key={`test-${index}`}
+          className={classes.root}
         >
-          <FiberManualRecordIcon className={clsx(classes.circleIcon,
-            submission.success ? classes.success : classes.error)}/>
-        </Typography>
+          <Box
+            className={classes.title}
+          >
+            <Typography
+              className={classes.iconWrapper}
+            >
+              <FiberManualRecordIcon className={clsx(classes.circleIcon,
+                test.result.passed === true ? classes.success : classes.error)}/>
+            </Typography>
 
-        <Typography
-          className={classes.name}
-        >
-          {submission.name}
-        </Typography>
-        <Typography
-          className={clsx(classes.testStatus, submission.success ? classes.success : classes.error)}
-        >
-          {submission.success ? 'Successful' : 'Failed'}
-        </Typography>
-      </Box>
-      <Typography className={classes.expand}>
-            Expand
-      </Typography>
-    </Box>
-
+            <Typography
+              className={classes.name}
+            >
+              {test.test.name}
+            </Typography>
+            <Typography
+              className={clsx(classes.testStatus, test.result.passed === true ? classes.success : classes.error)}
+            >
+              {test.result.passed === true ? 'Successful' : 'Failed'}
+            </Typography>
+          </Box>
+          <Typography className={classes.expand}>
+              Expand
+          </Typography>
+        </Box>
+      ))}
+    </React.Fragment>
   );
-};
-
-export default SubmissionTest;
+}
