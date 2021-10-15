671eadb3cd15bbff8c3eb7bbcdd720341e98a7a7
Maxwell Yang <gabrielyang233@outlook.com>
2021-10-14 23:54:40 -0400

    ADD course user number into reporter cronjob

--- api/jobs/reporter.py
+++ api/jobs/reporter.py
@@ -1,4 +1,6 @@
-from anubis.models import Course, User, TheiaSession
+from sqlalchemy import func
+
+from anubis.models import Course, User, TheiaSession, InCourse
 from anubis.utils.data import with_context
 
 @with_context
@@ -9,7 +11,7 @@ def get_courses():
 
 @with_context
 def get_active_users_this_semester():
-    return User.query.join(User.in_course).distinct().count()
+    return User.query.join(InCourse).distinct().count()
 
 @with_context
 def get_ides_opened_this_semester():
@@ -17,10 +19,19 @@ def get_ides_opened_this_semester():
         Course, TheiaSession.course_id==Course.id
     ).count()
 
+@with_context
+def get_course_user_count(course):
+    return User.query.join(
+        InCourse, User.id==InCourse.owner_id
+    ).join(
+        Course, Course.id==course.id
+    ).count()
+
 if __name__ == "__main__":
+    course_query = get_courses()
     # Course List
     print("Course Name\tCourse Code")
-    for course in get_courses():
+    for course in course_query:
         print(f"{course.name}\t{course.course_code}")
     
     print()
@@ -32,3 +43,10 @@ if __name__ == "__main__":
     # Number of IDEs opened this semester
     print("Total IDEs opened this semseter")
     print(get_ides_opened_this_semester())
+
+    print()
+    # Number of students for each course
+    print("Course Name\tCourse Code\tTotal users enrolled")
+    for course in course_query:
+        user_count = get_course_user_count(course)
+        print(f"{course.name}\t{course.course_code}\t{user_count}")
