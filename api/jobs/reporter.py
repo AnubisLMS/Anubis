2a5557b096f64856472a76372989db412dc80d58
Maxwell Yang <gabrielyang233@outlook.com>
2021-10-14 21:59:06 -0400

    ADD number of active users into reporter cronjob

--- api/jobs/reporter.py
+++ api/jobs/reporter.py
@@ -1,4 +1,4 @@
-from anubis.models import Course
+from anubis.models import Course, User
 from anubis.utils.data import with_context
 
 @with_context
@@ -7,7 +7,17 @@ def get_courses():
         Course.name != "TEST", Course.name != "DEMO"
     )
 
+@with_context
+def get_active_users_this_semester():
+    return User.query.join(User.in_course).distinct().count()
+
 if __name__ == "__main__":
+    # Course List
+    print("Course Name\tCourse Code")
     for course in get_courses():
-        print("Course Name\tCourse Code")
         print(f"{course.name}\t{course.course_code}")
+    
+    print()
+    # Number of users enrolled in at least on class this semester
+    print("Total users enrolled in at least one course this semester")
+    print(get_active_users_this_semester())
