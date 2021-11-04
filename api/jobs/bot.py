de4879b044388a60078a744febb458d5fecd14d5
levbernstein <lev2000@me.com>
2021-11-04 19:26:48 -0400

    ADD new commands for discord bot

--- api/jobs/bot.py
+++ api/jobs/bot.py
@@ -16,9 +16,7 @@ def get_courses() -> flask_sqlalchemy.BaseQuery:
 
     :return: A query object with TEST and DEMO course filtered
     """
-    return Course.query.filter(
-        Course.name != "TEST", Course.name != "DEMO"
-    )
+    return Course.query.filter(Course.name != "TEST", Course.name != "DEMO")
 
 
 @with_context
@@ -27,15 +25,15 @@ def get_active_users_this_semester() -> int:
     Count the number of users registered for at least one course this semester
 
     :return: The number of users registers for at least one course this
-             semester
+                                                                    semester
     """
-    return User.query.join(
-        InCourse, User.id == InCourse.owner_id
-    ).join(
-        Course, InCourse.course_id == Course.id
-    ).filter(
-        Course.name != "TEST", Course.name != "DEMO"
-    ).distinct().count()
+    return (
+        User.query.join(InCourse, User.id == InCourse.owner_id)
+        .join(Course, InCourse.course_id == Course.id)
+        .filter(Course.name != "TEST", Course.name != "DEMO")
+        .distinct()
+        .count()
+    )
 
 
 @with_context
@@ -45,9 +43,7 @@ def get_ides_opened_this_semester() -> int:
 
     :return: Total number of IDEs opened this semester
     """
-    return TheiaSession.query.join(
-        Course, TheiaSession.course_id == Course.id
-    ).count()
+    return TheiaSession.query.join(Course, TheiaSession.course_id == Course.id).count()
 
 
 @with_context
@@ -58,13 +54,12 @@ def get_course_user_count(course) -> int:
     :param course: The course object
     :return: Number of users registered for this course
     """
-    return User.query.join(
-        InCourse, User.id == InCourse.owner_id
-    ).join(
-        Course, Course.id == InCourse.course_id
-    ).filter(
-        Course.id == course.id
-    ).count()
+    return (
+        User.query.join(InCourse, User.id == InCourse.owner_id)
+        .join(Course, Course.id == InCourse.course_id)
+        .filter(Course.id == course.id)
+        .count()
+    )
 
 
 @with_context
@@ -75,75 +70,98 @@ def get_course_theia_session_count(course) -> int:
     :param course: The course object
     :return: Number of IDEs opened this semester
     """
-    return TheiaSession.query.join(
-        Course, TheiaSession.course_id == Course.id
-    ).filter(
-        Course.id == course.id
-    ).count()
+    return (
+        TheiaSession.query.join(Course, TheiaSession.course_id == Course.id)
+        .filter(Course.id == course.id)
+        .count()
+    )
 
 
 def generate_report() -> str:
     """
-    Generate a report of the statusesof Anubis. The statuses are:
-        Course names and code
-        Number of active users this semester
-        Number of IDEs opened this semester
-        Number of students for each course
-        Number of IDES opened for each course
-    
+    Generate a report of the statuses of Anubis. The statuses are:
+                                    Course names and code
+                                    Number of active users this semester
+                                    Number of IDEs opened this semester
+                                    Number of students for each course
+                                    Number of IDES opened for each course
+
     :return: The text of the report
     """
-    report = ""
-    course_query = get_courses()
-    # Course List
-    data = []
-    for course in course_query:
-        data.append([course.name, course.course_code])
-    report += tabulate(data, headers=["Course Name", "Course Code"])
 
-    report += "\n\n"
+    # Course List
+    courses = get_courses()
+    data = [[course.name, course.course_code] for course in courses]
+    report = tabulate(data, headers=["Course Name", "Course Code"])
+
     # Number of users enrolled in at least on class this semester
-    report += "Total users enrolled in at least one course this semester\n"
+    report += "\n\nTotal users enrolled in at least one course this semester\n"
     report += f"{get_active_users_this_semester()}\n"
 
-    report += "\n"
     # Number of IDEs opened this semester
-    report += "Total IDEs opened this semseter\n"
-    report += f"{get_ides_opened_this_semester()}\n"
+    report += "\nTotal IDEs opened this semseter\n"
+    report += f"{get_ides_opened_this_semester()}\n\n"
 
-    report += "\n"
-    # Number of students for each course
-    data = []
-    for course in course_query:
-        user_count = get_course_user_count(course)
-        data.append([course.name, course.course_code, user_count])
+    # Number of students, IDEs for each course
+    data = [
+        [
+            course.name,
+            course.course_code,
+            get_course_user_count(course),
+            get_course_theia_session_count(course),
+        ]
+        for course in courses
+    ]
     report += tabulate(
-        data,
-        headers=["Course Name", "Course Code", "Total users enrolled"]
+        data, headers=["Course Name", "Course Code", "Users enrolled", "IDES opened"]
     )
-
-    report += "\n\n"
-    # Number of IDEs opened for each course
-    data = []
-    for course in course_query:
-        theia_count = get_course_theia_session_count(course)
-        data.append([course.name, course.course_code, theia_count])
-    report += tabulate(data, headers=["Course Name", "Course Code", "Total IDEs opened"])
 
     return report
 
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(
+    command_prefix="!",
+    case_insensitive=True,
+    help_command=None,
+    owner_id=448648593314807809,
+)
 
 
-@bot.command(name="report", help="Anubis Usage Report")
-async def report(ctx):
+@bot.command(name="report", help="Anubis usage report")
+async def report(ctx, *args):
     """
     Respond to `!report` command with a report of the statuses of Anubis
 
     :return:
     """
     await ctx.send("```" + generate_report() + "```")
+
+
+@bot.command(name="contribute", aliases=("github",), help="Contributing to Anubis")
+async def contribute(ctx, *args):
+    """
+    Respond to `!contribute` command with a link to the GitHub repo
+
+    :return:
+    """
+    emb = discord.Embed(
+        title="Contributing to Anubis",
+        description="Thanks for your interest in contributing to Anubis! We're always looking for new people to help make Anubis even better. Please head to [our GitHub repo](https://github.com/anubislms/Anubis).",
+    ).set_thumbnail(url=bot.user.avatar_url)
+    await ctx.send(embed=emb)
+
+
+@bot.command(name="help", aliases=("commands",), help="Shows you this list")
+async def helpCommand(ctx, *args):
+    """
+    Respond to `!help` command with a list of valid commands and their descriptions
+
+    :return:
+    """
+    emb = discord.Embed(title="Anubis Bot Help", description="")
+    for command in bot.commands:
+        emb.add_field(name=command.name, value=command.help, inline=True)
+    await ctx.send(embed=emb)
 
 
 if __name__ == "__main__":
