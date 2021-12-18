805adaa2fc142079ade1954f7a54aafe02edd3c8
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2021-12-18 17:01:38 -0500

    ADD button to assignment page for regrading reaped assignments

--- old-web/src/Components/Admin/Assignment/RegradeWarning.jsx
+++ old-web/src/Components/Admin/Assignment/RegradeWarning.jsx
@@ -54,6 +54,10 @@ export default function RegradeWarning({warningOpen, setWarningOpen, regradeAssi
           title={'Regrade all'}
         />
         <RegradeButton
+          title={'Regrade Reaped'}
+          params={{reaped: 1}}
+        />
+        <RegradeButton
           title={'Regrade last 6 hours'}
           params={{hours: '6'}}
         />
