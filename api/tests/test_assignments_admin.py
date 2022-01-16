c4b5d563fdcfbf430cdd86cf471ceafab0d46fde
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-01-15 22:19:58 -0500

    FIX sample assignment sync test

--- api/tests/test_assignments_admin.py
+++ api/tests/test_assignments_admin.py
@@ -5,28 +5,13 @@ from utils import Session, permission_test
 sample_sync = {
     "name": "CS-UY 3224 TEST ADMIN",
     "course": "CS-UY 3224",
-    "hidden": True,
-    "github_template": "wabscale/xv6-public",
-    "github_repo_required": True,
     "unique_code": "aa11bb22",
     "pipeline_image": "registry.digitalocean.com/anubis/assignment/aa11bb2233",
-    "date": {
-        "release": str(datetime.now() - timedelta(hours=2)),
-        "due": str(datetime.now() + timedelta(hours=12)),
-        "grace": str(datetime.now() + timedelta(hours=13)),
-    },
-    "description": "This is a very long description that encompasses the entire assignment\n",
-    "questions": [
-        {
-            "pool": 1,
-            "questions": [
-                {"q": "What is 3*4?", "a": "12"},
-                {"q": "What is 3*2", "a": "6"},
-            ],
-        },
-        {"pool": 2, "questions": [{"q": "What is sqrt(144)?", "a": "12"}]},
+    "tests": [
+        {"name": "a1", "hidden": False, "points": 10},
+        {"name": "b2", "hidden": True, "points": 10},
+        {"name": "c3", "hidden": False, "points": 20},
     ],
-    "tests": ["abc123"],
 }
 
 
