15e35e164d9858fcdcbcb73acbbddca676c7ea1b
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-02-06 16:31:27 -0500

    FIX import issue

--- api/anubis/rpc/safety_nets.py
+++ api/anubis/rpc/safety_nets.py
@@ -1,6 +1,5 @@
 import functools
 from anubis.utils.data import is_job
-from anubis.rpc.enqueue import enqueue_create_assignment_github_repo
 
 
 def create_repo_safety_net(func):
@@ -8,6 +7,7 @@ def create_repo_safety_net(func):
     def wrapper(*args, **kwargs):
         repos, errors = func(*args, **kwargs)
         if len(errors) > 0 and not is_job():
+            from anubis.rpc.enqueue import enqueue_create_assignment_github_repo
             enqueue_create_assignment_github_repo(*args)
         return repos, errors
     return wrapper
