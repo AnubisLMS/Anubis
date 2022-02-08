58cbdccc6ff5c6e7b35ba2cd9664b9e419ff8c67
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-02-07 23:42:32 -0500

    CHG adjust sentry

--- api/anubis/utils/sentry.py
+++ api/anubis/utils/sentry.py
@@ -4,7 +4,19 @@ from anubis.utils.data import is_debug, is_job
 from anubis.utils.logging import logger
 
 
-def add_sentry(_: Flask):
+def traces_sampler(sampling_context):
+    # Examine provided context data (including parent decision, if any)
+    # along with anything in the global namespace to compute the sample rate
+    # or sampling decision for this transaction
+
+    if 'REQUEST_URI' in sampling_context and sampling_context['REQUEST_URI'] == '/':
+        return 0
+    else:
+        # Default sample rate
+        return 0.25
+
+
+def add_sentry(app: Flask):
     if not is_debug() and env.SENTRY_DSN != '' and env.SENTRY_DSN is not None:
         import sentry_sdk
         from sentry_sdk.integrations.flask import FlaskIntegration
@@ -18,12 +30,13 @@ def add_sentry(_: Flask):
             # Set traces_sample_rate to 1.0 to capture 100%
             # of transactions for performance monitoring.
             # We recommend adjusting this value in production.
-            traces_sample_rate=1.0
+            traces_sample_rate=1.0,
 
             # By default the SDK will try to use the SENTRY_RELEASE
             # environment variable, or infer a git commit
             # SHA as release, however you may want to set
             # something more human-readable.
             # release="myapp@1.0.0",
+            traces_sampler=traces_sampler,
         )
 
