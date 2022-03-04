ccc8c6d2fed2c28274b744a84df8f80a89b57022
Zixuan James Li <359101898@qq.com>
2022-03-04 13:47:03 -0500

    FIX display issue of question responses in for the admin view
    Ensure that the text editor correctly reads the response from
    the question object and make it readonly in autograde result.
    
    Fixes #347

--- web/src/components/core/Questions/QuestionEditor.jsx
+++ web/src/components/core/Questions/QuestionEditor.jsx
@@ -38,6 +38,7 @@ export default function QuestionEditor({question, updateResponse, saveResponse})
   const [showSolution, setShowSolution] = useState(false);
   const response = question?.response?.text ?? '';
   const mode = question?.question?.code_language || 'markdown';
+  const readOnly = !updateResponse || !saveResponse;
 
   return (
     <Box className={classes.root}>
@@ -70,17 +71,26 @@ export default function QuestionEditor({question, updateResponse, saveResponse})
       <Typography variant={'subtitle1'} className={classes.subTitle}>
         Your Answer:
       </Typography>
-      <AceEditor
-        mode={mode}
-        theme="monokai"
-        value={response}
-        onChange={updateResponse}
-        commands={[{ // commands is array of key bindings.
-          name: 'save', // name for the key binding.
-          bindKey: {win: 'Ctrl-s', mac: 'Command-s'}, // key combination used for the command.
-          exec: saveResponse, // function to execute when keys are pressed.
-        }]}
-      />
+      {readOnly ? (
+        <AceEditor
+          mode={mode}
+          theme="monokai"
+          value={response}
+          readOnly
+        />
+      ) : (
+        <AceEditor
+          mode={mode}
+          theme="monokai"
+          value={response}
+          onChange={updateResponse}
+          commands={[{ // commands is array of key bindings.
+            name: 'save', // name for the key binding.
+            bindKey: {win: 'Ctrl-s', mac: 'Command-s'}, // key combination used for the command.
+            exec: saveResponse, // function to execute when keys are pressed.
+          }]}
+        />
+      )}
     </Box>
   );
 }
--- web/src/components/core/Questions/QuestionsCard.jsx
+++ web/src/components/core/Questions/QuestionsCard.jsx
@@ -21,8 +21,6 @@ import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import AssignmentLateIcon from '@material-ui/icons/AssignmentLate';
 
 import QuestionEditor from './QuestionEditor';
-import standardStatusHandler from '../../../utils/standardStatusHandler';
-import standardErrorHandler from '../../../utils/standardErrorHandler';
 
 import 'ace-builds/src-min-noconflict/theme-monokai';
 import 'ace-builds/src-min-noconflict/mode-c_cpp';
@@ -52,86 +50,48 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const saveResponse = (id, responses, index, setResponses, enqueueSnackbar) => () => {
-  axios.post(`/api/public/questions/save/${id}`, {response: responses[index].text}).then((resp) => {
-    const data = standardStatusHandler(resp, enqueueSnackbar);
-    if (data?.response) {
-      setResponses((prev) => {
-        prev[index] = data.response;
-        return [...prev];
-      });
-    }
-  }).catch(standardErrorHandler(enqueueSnackbar));
-};
-
 export default function QuestionsCard({questions}) {
   const classes = useStyles();
-  const {enqueueSnackbar} = useSnackbar();
-  const [responses, setResponses] = useState(null);
 
-  React.useEffect(() => {
-    setResponses(questions.map((question) => question.response));
-  }, [questions]);
-
-  if (!questions || questions.length === 0 || !responses || responses.length === 0) {
+  if (!questions || questions.length === 0) {
     return null;
   }
-
-  const updateResponse = (index) => (value) => {
-    setResponses((state) => {
-      state[index].text = value;
-      return [...state];
-    });
-  };
 
   return (
     <div className={classes.root}>
       <Grid container justify={'center'} spacing={1}>
 
-        {questions.map(({
-          id, question,
-        }, index) => (
-          <Grid item xs={12} key={`question-${question.pool}`} className={classes.question}>
+        {questions.map((question) => (
+          <Grid item xs={12} key={`question-${question.question.pool}`} className={classes.question}>
             <Accordion>
               <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
                 <div className={classes.icon}>
-                  {responses[index].submitted === null ? (
+                  {!question.response || question.response.submitted === null ? (
                     <Tooltip title={'No Submission'}>
                       <RemoveCircleOutlineIcon style={{color: grey[500]}}/>
                     </Tooltip>
                   ) : (
-                    responses[index].late ? (
-                      <Tooltip title={`Submitted late. Last modified ${responses[index].submitted}`}>
+                    question.response.late ? (
+                      <Tooltip title={`Submitted late. Last modified ${question.response.submitted}`}>
                         <AssignmentLateIcon style={{color: red[500]}}/>
                       </Tooltip>
                     ) : (
-                      <Tooltip title={`Submitted on time. Last modified ${responses[index].submitted}`}>
+                      <Tooltip title={`Submitted on time. Last modified ${question.response.submitted}`}>
                         <AssignmentTurnedInIcon style={{color: grey[500]}}/>
                       </Tooltip>
                     )
                   )}
                 </div>
                 <Typography className={classes.heading}>
-                  Question {question.pool}
+                  Question {question.question.pool}
                 </Typography>
               </AccordionSummary>
 
               <AccordionDetails>
                 {/* Question content */}
                 <Grid container spacing={2} direction={'column'}>
-                  <ReactMarkdownWithHtml
-                    className={classes.markdown}
-                    plugins={[gfm]}
-                    allowDangerousHtml
-                  >
-                    {question.question}
-                  </ReactMarkdownWithHtml>
-
                   <QuestionEditor
                     question={question}
-                    response={responses[index].text}
-                    updateResponse={updateResponse(index)}
-                    saveResponse={saveResponse(id, responses, index, setResponses, enqueueSnackbar)}
                   />
                 </Grid>
               </AccordionDetails>
