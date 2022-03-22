632e41af24c11c3e2b40379d05cf807c5be34b12
Aaron Chen <aaron.chen11219@gmail.com>
2022-03-21 22:23:12 -0400

    CHG Lectures.jsx to follow the format of other public pages. Add LectureItem.jsx and LectureItem.styles.jsx with component LectureItem to follow format of other public pages. TESTED with sample data. Only thing to fix is the file attachment

---
+++ web/src/components/core/LectureItem/LectureItem.styles.jsx
@@ -0,0 +1,13 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+  postTimeText: {
+    color: theme.palette.white,
+  },
+  green: {
+    color: theme.palette.color.green,
+  },
+  red: {
+    color: theme.palette.color.red,
+  },
+}));
--- web/src/pages/core/public/Lectures/Lectures.jsx
+++ web/src/pages/core/public/Lectures/Lectures.jsx
@@ -7,11 +7,15 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
 import {useStyles} from './Lectures.styles';
-import StandardLayout from '../../../../components/shared/Layouts/StandardLayout';
 import useQuery from '../../../../hooks/useQuery';
 import standardErrorHandler from '../../../../utils/standardErrorHandler';
 import standardStatusHandler from '../../../../utils/standardStatusHandler';
 import Typography from '@material-ui/core/Typography';
+import StandardLayout from '../../../../components/shared/Layouts/StandardLayout';
+import SectionHeader from '../../../../components/shared/SectionHeader/SectionHeader';
+import ListHeader from '../../../../components/shared/ListHeader/ListHeader';
+import Divider from '../../../../components/shared/Divider/Divider';
+import LectureItem from '../../../../components/core/LectureItem/LectureItem';
 
 const Lectures = () => {
   const classes = useStyles();
@@ -31,45 +35,62 @@ const Lectures = () => {
   }, []);
 
   const get_href = (row) => (
-    `${window.location.origin}/api/public/static${row.static_file.path}/${row.static_file.filename}`
+    `/api/public/static${row.static_file.path}/${row.static_file.filename}`
   );
-
+  console.log(lectures);
   return (
-    <StandardLayout title={'Lectures'} description={'Lecture notes posted by professors'}>
-      <Grid container spacing={1} justify={'center'}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            <DataGrid
-              columns={[
-                {field: 'course', headerName: 'Course', width: 150},
-                {field: 'post_time', headerName: 'Post Time', width: 170},
-                {field: 'title', headerName: 'Title', width: 300},
-                {
-                  field: 'a', headerName: 'Lecture Attachment', width: 300, renderCell: ({row}) => (
-                    <div>
-                      <Typography
-                        variant={'body1'}
-                        color={'primary'}
-                        style={{display: 'inline'}}
-                        component={'a'}
-                        target={'_blank'}
-                        href={get_href(row)}
-                      >
-                        {row.static_file.filename}
-                      </Typography>
-                    </div>
-                  ),
-                },
-              ]}
-              rows={lectures}
-              rowsPerPageOptions={[10, 20, 30]}
-              pagination
-            />
-          </Paper>
-        </Grid>
-      </Grid>
+    <StandardLayout>
+      <SectionHeader isPage title = 'Lectures'/>
+      <Divider />
+      <ListHeader sections = {['Course', 'Post Time', 'Title', 'Lecture Attachment']} />
+      {lectures.map((lecture, index) => (
+        <LectureItem
+          key = {`${lecture.name}-${index}`}
+          course = {lecture.course}
+          postTime = {lecture.post_time}
+          title = {lecture.title}
+          id = {lecture.id}
+          fileAttachment = {get_href(lecture)}
+        />
+      ))}
     </StandardLayout>
   );
 };
+//     <StandardLayout title={'Lectures'} description={'Lecture notes posted by professors'}>
+//       <Grid container spacing={1} justify={'center'}>
+//         <Grid item xs={12}>
+//           <Paper className={classes.paper}>
+//             <DataGrid
+//               columns={[
+//                 {field: 'course', headerName: 'Course', width: 150},
+//                 {field: 'post_time', headerName: 'Post Time', width: 170},
+//                 {field: 'title', headerName: 'Title', width: 300},
+//                 {
+//                   field: 'a', headerName: 'Lecture Attachment', width: 300, renderCell: ({row}) => (
+//                     <div>
+//                       <Typography
+//                         variant={'body1'}
+//                         color={'primary'}
+//                         style={{display: 'inline'}}
+//                         component={'a'}
+//                         target={'_blank'}
+//                         href={get_href(row)}
+//                       >
+//                         {row.static_file.filename}
+//                       </Typography>
+//                     </div>
+//                   ),
+//                 },
+//               ]}
+//               rows={lectures}
+//               rowsPerPageOptions={[10, 20, 30]}
+//               pagination
+//             />
+//           </Paper>
+//         </Grid>
+//       </Grid>
+//     </StandardLayout>
+//   );
+// };
 
 export default Lectures;
