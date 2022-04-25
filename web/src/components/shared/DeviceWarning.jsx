c942992db009dde811059af03e7e95ca62d102a2
AcKindle3 <jychen630@outlook.com>
2022-04-25 19:25:51 -0400

    FIX disable warning msg on iPadOS browser
    Signed-off-by: Junyao Chen <jychen630@outlook.com>

--- web/src/components/shared/DeviceWarning.jsx
+++ web/src/components/shared/DeviceWarning.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import {browserName, isBrowser, isChrome, isFirefox} from 'react-device-detect';
+import {browserName, isChrome, isDesktop, isFirefox, isTablet} from 'react-device-detect';
 import {useSnackbar} from 'notistack';
 
 export default function DeviceWarning() {
   const {enqueueSnackbar} = useSnackbar();
 
   React.useEffect(() => {
-    if ((isChrome || isFirefox) && isBrowser) {
-      return;
-    }
-
-    if (!(isChrome || isFirefox)) {
+    const acceptDevice = isDesktop || isTablet;
+    const acceptBrowser = isChrome || isFirefox;
+    if (!acceptBrowser) {
       enqueueSnackbar(`We noticed you are on ${browserName}. ` +
         'Some things may not work as intended. ' +
         'Try Firefox or Chrome if you run into any issues!', {variant: 'warning'});
       return;
     }
-
-    if (!isBrowser) {
+    if (!acceptDevice) {
       enqueueSnackbar('The Anubis website may not work well ' +
-        'outside of a desktop browser!', {variant: 'warning'});
+        'outside of a desktop or tablet browser!', {variant: 'warning'});
     }
   }, []);
 
