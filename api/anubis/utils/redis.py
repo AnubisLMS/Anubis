6307bc9c9287a44adcd179620af232a5d3b1f27b
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-09-22 23:07:42 -0400

    FIX redis on mindebug

--- api/anubis/utils/redis.py
+++ api/anubis/utils/redis.py
@@ -1,7 +1,11 @@
 from redis import Redis
 from anubis.env import env
 
-redis = Redis(
-    host=env.CACHE_REDIS_HOST,
-    password=env.CACHE_REDIS_PASSWORD,
-)
+match env.CACHE_TYPE:
+    case "RedisCache":
+        redis = Redis(
+            host=env.CACHE_REDIS_HOST,
+            password=env.CACHE_REDIS_PASSWORD,
+        )
+    case _:
+        redis = None
