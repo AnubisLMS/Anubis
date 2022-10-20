57fb2e319c08e7a55326708f4b9a3e98deb7c77e
Chliu88 <cl5522@nyu.edu>
2022-10-19 22:58:31 -0400

    ADD inserted git hash into react frontend

--- web/src/components/shared/Copyright.jsx
+++ web/src/components/shared/Copyright.jsx
@@ -3,6 +3,7 @@ import Link from '@mui/material/Link';
 import React from 'react';
 
 export default function Copyright() {
+  const {REACT_APP_GIT_TAG} = process.env
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
@@ -10,6 +11,8 @@ export default function Copyright() {
         Memes
       </Link>{' '}
       {new Date().getFullYear()}
+      {' '}
+      {REACT_APP_GIT_TAG}
     </Typography>
   );
 }
