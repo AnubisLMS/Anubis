973ab4b661116493d78816392dc8091e3537d4db
Tim Chen <ytc344@nyu.edu>
2022-11-03 15:08:06 -0400

    FIX format should be ttf not woff2

--- web/src/theme/Theme.jsx
+++ web/src/theme/Theme.jsx
@@ -16,7 +16,7 @@ const fira = {
     'U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, ' +
     'U+2191, U+2193, U+2212, U+2215, U+FEFF',
 };
-// 
+//
 
 let theme = createTheme({
   palette: {
@@ -59,13 +59,13 @@ let theme = createTheme({
           font-style: normal; 
           font-display: swap;
           font-weight: 400;
-          src: local('Fira Code'), url(${FiraRegular}) format('woff2');
+          src: local('Fira Code'), url(${FiraRegular}) format('ttf');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, 
           U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, 
           U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }  
-      `
-    }
+      `,
+    },
   },
   shape: {
     borderRadius: 8,
