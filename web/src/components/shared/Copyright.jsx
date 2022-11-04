326427edf90f10bafc9679e04855812ec9544390
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-11-04 18:39:44 -0400

    CHG clean up frontend git commit

--- web/src/components/shared/Copyright.jsx
+++ web/src/components/shared/Copyright.jsx
@@ -1,18 +1,22 @@
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import React from 'react';
 
 export default function Copyright() {
-  const {REACT_APP_GIT_TAG} = process.env
+  const {REACT_APP_GIT_TAG} = process.env;
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link href={'/api/public/memes/'} target={'_blank'}>
+    <Box>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link href={'/api/public/memes/'} target={'_blank'}>
         Memes
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {' '}
-      {REACT_APP_GIT_TAG}
-    </Typography>
+        </Link>{' '}
+        {new Date().getFullYear()}
+      </Typography>
+      <Typography variant={'body2'} color={'textSecondary'} sx={{fontSize: 10}}>
+        {REACT_APP_GIT_TAG}
+      </Typography>
+    </Box>
   );
 }
--- web/src/components/shared/Footer.jsx
+++ web/src/components/shared/Footer.jsx
@@ -5,12 +5,12 @@ import {Route, Switch} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    paddingBottom: theme.spacing(2),
+    paddingBottom: theme.spacing(3),
     bottom: '0',
     left: '0',
     textAlign: 'center',
     position: 'fixed',
-    height: '30px',
+    height: '40px',
     width: '100%',
   },
 }));
