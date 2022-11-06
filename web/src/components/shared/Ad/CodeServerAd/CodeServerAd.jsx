e8c37acb71b3a4a220dae09e57b567cc4d0db6db
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-11-06 18:14:56 -0500

    ADD code-server ad to playgrounds

---
+++ web/src/components/shared/Ad/CodeServerAd/CodeServerAd.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Button from '@mui/material/Button';
+import Zoom from '@mui/material/Zoom';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Typography from '@mui/material/Typography';
+
+export default function CodeServerAd() {
+  return (
+    <Zoom in>
+      <Alert
+        severity="success"
+        variant="outlined"
+        sx={{mt: 2, mb: 2}}
+        action={
+          <Button
+            sx={{m: 2}}
+            variant={'contained'}
+            color={'primary'}
+            startIcon={<ExitToAppIcon/>}
+            href="https://github.com/AnubisLMS/IDE/issues/new"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open Issue
+          </Button>
+        }
+      >
+        <AlertTitle sx={{fontSize: 22}}>
+          <strong>
+            {'You may notice some changes...'}
+          </strong>
+        </AlertTitle>
+        <Typography variant={'body1'}>
+          We are in the progress of upgrading the IDE servers that we use from
+          Theia to Code-Server. We are making this change so that we can better accommodate
+          vscode extensions. There will be some visual changes that you will notice right away.{' '}
+          <i>If you find that anything now doesn&#39;t work, please feel free to open an issue on
+          our IDE repo on github.</i>
+        </Typography>
+      </Alert>
+    </Zoom>
+  );
+}
