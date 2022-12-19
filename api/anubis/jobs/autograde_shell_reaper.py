36c69e0b3245a1f9934b29164c3ac4010d168b65
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-12-18 22:20:32 -0500

    CHG adjust shell autograde reaper to use recent not active assignments

--- api/anubis/jobs/autograde_shell_reaper.py
+++ api/anubis/jobs/autograde_shell_reaper.py
@@ -1,13 +1,13 @@
 from anubis.constants import REAPER_TXT
-from anubis.lms.assignments import get_active_assignments
+from anubis.lms.assignments import get_recent_assignments
 from anubis.lms.shell_autograde import autograde_shell_assignment_sync
-from anubis.models import Assignment
 from anubis.utils.data import with_context
 from anubis.utils.logging import logger
 
+
 @with_context
 def autograde_shell_sync():
-    active_assignments = get_active_assignments(Assignment.shell_autograde_enabled == True)
+    active_assignments = get_recent_assignments()
 
     logger.info(f'{active_assignments=}')
 
