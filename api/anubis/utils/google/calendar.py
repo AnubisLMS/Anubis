8a070ee88046f0489a73d95028c347b8da2f7fc5
zphw <zph@zph.io>
2022-12-19 02:46:30 -0500

    ADD APIs for Google Cal secondary calendar

--- api/anubis/utils/google/calendar.py
+++ api/anubis/utils/google/calendar.py
@@ -18,6 +18,58 @@ def get_calendar_service() -> googleapiclient.discovery.Resource:
     )
 
 
+def add_calendar(
+    calendar: dict
+) -> str:
+    """Add a secondary calendar.
+
+    Args:
+      calendar: The calendar to be added
+
+    Returns:
+      The identifier of the added calendar
+    """
+
+    service = get_calendar_service()
+
+    logger.debug(f"ADDING CALENDAR {json.dumps(calendar)}")
+
+    try:
+        calendar = service.calendars().insert(body=calendar).execute()
+        return calendar['id']
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        if raise_:
+            raise e
+        return False
+
+
+def delete_calendar(
+    calendar_id: str
+) -> str:
+    """Delete a secondary calendar.
+
+    Args:
+      calendar_id: The calendar to be deleted
+
+    Returns:
+      The identifier of the deleted calendar
+    """
+
+    service = get_calendar_service()
+
+    logger.debug(f"DELETING CALENDAR {calendar_id}")
+
+    try:
+        service.calendars().delete(calendarId=calendar_id).execute()
+        return calendar['id']
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        if raise_:
+            raise e
+        return False
+
+
 def add_event(
     calendar_id: str="primary",
     event: dict,
@@ -25,7 +77,7 @@ def add_event(
     """Add a Google Calendar event.
 
     Args:
-      calendar_id: The calendarId to which the event is added
+      calendar_id: The calendarId to which the event will be added
       event:       The event to be added
 
     Returns:
@@ -39,6 +91,34 @@ def add_event(
     try:
         event = service.events().insert(calendarId=calendar_id, body=event).execute()
         return event.get('id')
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        if raise_:
+            raise e
+        return False
+
+
+def delete_event(
+    calendar_id: str="primary",
+    event_id: str,
+) -> str:
+    """Delete a Google Calendar event.
+
+    Args:
+      calendar_id: The calendarId to which the event will be deleted
+      event_id:       The event to be deleted
+
+    Returns:
+      The identifier of the deleted event
+    """
+
+    service = get_calendar_service()
+
+    logger.debug(f"DELETING CALENDAR EVENT {event_id} in {calendar_id}")
+
+    try:
+        service.events().delete(calendarId=calendar_id, eventId=event_id).execute()
+        return event_id
     except Exception as e:
         logger.error(traceback.format_exc())
         if raise_:
