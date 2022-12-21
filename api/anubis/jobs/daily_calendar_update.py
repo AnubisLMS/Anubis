576a800ba1291d6b2ff0b7edecd4e12d28ab3280
Chliu88 <cl5522@nyu.edu>
2022-12-21 18:04:17 -0500

    ADD made some comments more understandable

--- api/anubis/jobs/daily_calendar_update.py
+++ api/anubis/jobs/daily_calendar_update.py
@@ -13,6 +13,13 @@ Implementation:
     - if deleted, delete an event
 
 **Not sure if anubis has an "alter due date" functionality**
+
+Functions needed:
+    - insert event into calendar
+    - delete event in calendar
+    - get dictionary with this format:
+        - {<course_id1>: [<assignment1>,<assignment2>,...], <course_id2>: [<assignment1>,<assignment2>,...], ...}
+        - this is for comparing calendar course assignments to assignments gotten from Anubis database
 """
 
 from datetime import datetime, timedelta
@@ -24,7 +31,9 @@ from anubis.lms.courses import get_course_users, get_active_courses, get_active_
 from anubis.models import Assignment, Course, User
 from anubis.utils.data import with_context
 from anubis.utils.logging import logger
-import anubis.jobs.calendar_jobs # Need: list of assignemts per course, insert event/delete event to a specific calendar
+import anubis.jobs.calendar_jobs 
+# functions needed: 
+# insert event/delete event to a specific calendar
 
 now = datetime.now()
 
@@ -33,7 +42,7 @@ now = datetime.now()
 course_list = get_active_courses()
 assignment_list = get_active_assignments() # current anubis assignments state
 
-# create dictionary with courses as keys indexing lists of their assignments
+# create dictionary with courses.id as keys indexing lists of their assignments
 course_assignment_list = {}
 for course in course_list:
     course_assignment_mapping[course.id] = []
@@ -41,15 +50,16 @@ for course in course_list:
         if course.id == assignment.course_id:
             course_assignment_mapping[course.id].append(assignment)
 
-# get list of assignments per course
+# get dictionary with course.id as keys indexing lists of their assignments from existing calendars
+# ex. {<course_id1>:[<assignment1>,<assignment2>,...], <course_id2>: [<assignment1>,<assignment2>,...], ...}
 
 new_assignmnets = []
 deleted_assignments = []
 
 def check_assignment_updates_for_class():
 
-    # FOR EACH course calendar, loop through returned calendar list of courses
-    # compare assignment lists with respective course in course_assignment_list dictionary
+    # FOR EACH course calendar:
+    # compare assignment lists with respective course with matching course.id in course_assignment_list dictionary
     # append new assignments to new_assignments
     # append deleted_assignments to deleted_assignments
 
@@ -59,7 +69,7 @@ def insert_assignments():
 
     # loop through new_assignments
     # use insert_assignment function to insert assignment to calendar
-    
+
     return 0
 
 def delete_assignment():
