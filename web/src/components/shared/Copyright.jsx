6cf183a26d0bf0bcb87edbb60747e0a64bd9404c
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-12-23 16:33:47 -0500

    FIX web git tag docker caching

--- web/src/components/shared/Copyright.jsx
+++ web/src/components/shared/Copyright.jsx
@@ -4,7 +4,6 @@ import Link from '@mui/material/Link';
 import React from 'react';
 
 export default function Copyright() {
-  const {REACT_APP_GIT_TAG} = process.env;
   return (
     <Box>
       <Typography variant="body2" color="textSecondary" align="center">
@@ -15,7 +14,7 @@ export default function Copyright() {
         {new Date().getFullYear()}
       </Typography>
       <Typography variant={'body2'} color={'textSecondary'} sx={{fontSize: 10}}>
-        {REACT_APP_GIT_TAG}
+        GIT_TAG_PLACEHOLDER
       </Typography>
     </Box>
   );
