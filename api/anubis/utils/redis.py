66020fc9a9dd33aa7a948034a5fc8baff77e5697
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2023-01-23 09:44:59 -0500

    FIX env reference for redis lock

--- api/anubis/utils/redis.py
+++ api/anubis/utils/redis.py
@@ -6,8 +6,8 @@ from anubis.env import env
 match env.CACHE_TYPE:
     case "RedisCache":
         redis = Redis(
-            host=env.CACHE_REDIS_HOST,
-            password=env.CACHE_REDIS_PASSWORD,
+            host=env.REDIS_HOST,
+            password=env.REDIS_PASS,
         )
     case _:
         redis = None
