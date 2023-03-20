b1f8f321d0c49733319d7ea8008a51f1fcddb4ea
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2023-03-20 05:22:59 -0400

    RM ad

--- web/src/components/shared/Ad/GenericAd/GenericAd.jsx
+++ web/src/components/shared/Ad/GenericAd/GenericAd.jsx
@@ -7,18 +7,22 @@ import Button from '@mui/material/Button';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 
-const data = {
-  severity: 'warning',
-  title: 'Maintenance incoming',
-  content: 'Anubis will be unavailable briefly Sunday the 19th for about an hour starting at 11PM while we do ' +
-    'some minor cluster updates (2023-03-19 23:00:00 EDT - 2023-03-20 00:00:00 EDT).',
-  // action: {
-  //   href: 'https://discord.gg/GEYtaATJHs',
-  //   label: 'OSIRIS discord',
-  // },
-};
+// const data = {
+//   severity: 'warning',
+//   title: 'Maintenance incoming',
+//   content: 'Anubis will be unavailable briefly Sunday the 19th for about an hour starting at 11PM while we do ' +
+//     'some minor cluster updates (2023-03-19 23:00:00 EDT - 2023-03-20 00:00:00 EDT).',
+//   // action: {
+//   //   href: 'https://discord.gg/GEYtaATJHs',
+//   //   label: 'OSIRIS discord',
+//   // },
+// };
+const data = false;
 
 export default function GenericAd() {
+  if (!data) {
+    return null;
+  }
   return (
     <Zoom in>
       <Alert
