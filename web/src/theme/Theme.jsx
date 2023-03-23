2c8551513150e24cb94e02dd830ea7aecf2b3915
KHVIII <ytc344@nyu.edu>
2023-03-22 22:45:10 -0400

    CHG Fira Code Font Loads From Google Fonts Instead Of Locally

--- web/src/theme/Theme.jsx
+++ web/src/theme/Theme.jsx
@@ -1,5 +1,4 @@
 import {createTheme} from '@mui/material/styles';
-import FiraRegular from '../assets/fonts/FiraCode-Regular.ttf';
 
 
 const fira = {
@@ -7,10 +6,6 @@ const fira = {
   fontStyle: 'normal',
   fontDisplay: 'swap',
   fontWeight: 400,
-  src: `
-    local('Fira Code'),
-    url(${FiraRegular}) format('ttf')
-  `,
   unicodeRange:
     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, ' +
     'U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, ' +
