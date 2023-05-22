e2bb4f9b999db2f3517ce7c81b28e9641c55fa81
dolf321 <rami.r.richani02@hotmail.com>
2023-05-22 17:38:42 +0400

    ADD regrading tests
    Signed-off-by: John McCann Cunniff Jr <johncunniff1248@gmail.com>

--- api/tests/test_regrade_admin.py
+++ api/tests/test_regrade_admin.py
@@ -1,7 +1,7 @@
 from utils import Session, permission_test, with_context
 
+import pytest, time
 from anubis.models import Submission
-
 
 @with_context
 def get_student_submission_commit(assignment_ids):
@@ -13,7 +13,6 @@ def get_student_submission_commit(assignment_ids):
         if submission is not None:
             return submission.commit, assignment_id
 
-
 def test_regrade_admin():
     superuser = Session("superuser")
     assignments = superuser.get("/admin/assignments/list")["assignments"]
@@ -23,3 +22,30 @@ def test_regrade_admin():
     permission_test(f"/admin/regrade/status/{assignment_id}")
     permission_test(f"/admin/regrade/submission/{commit}")
     permission_test(f"/admin/regrade/assignment/{assignment_id}")
+
+@pytest.mark.timeout(40)
+def test_regrade_admin_student():
+    superuser = Session("superuser")
+    # Get list of students
+    students = superuser.get("/admin/students/list")["students"]
+    assert len(students) > 0
+    student_netid = students[0]["netid"]
+
+    # Permission tests
+    permission_test(f"/admin/regrade/student/{student_netid}")
+    permission_test(f"/admin/regrade/status/student/{student_netid}")
+
+    # Test Submissions pipeline
+    resp = superuser.get(f"/admin/regrade/student/{student_netid}")
+    assert resp["status"] == "Regrade enqueued." 
+    
+    # get the status of the regrade for a student and make sure they get fully procesed
+    status = superuser.get(f"/admin/regrade/status/student/{student_netid}")
+
+    # make sure total is greater than 0 (meaning it has actually been enqueued)
+    assert status["total"] > 0
+    
+    while (status["processed"] !=status["total"]):
+        time.sleep(5)
+        status = superuser.get(f"/admin/regrade/status/student/{student_netid}")
+    assert status["processed"] == status["total"]
