e0a1e58cebd702932fbf1e451dec3461b8da2a0f
dolf321 <rami.r.richani02@hotmail.com>
2023-07-16 14:23:52 +0300

    ADD Writing a post functionality

--- web/src/components/forums/CreateDialog/CreateDialog.jsx
+++ web/src/components/forums/CreateDialog/CreateDialog.jsx
@@ -1,61 +1,102 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
 import Box from '@mui/material/Box';
 import Input from '@mui/material/Input';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Switch from '@mui/material/Switch';
+import EditorToolbar from './Toolbar/EditorToolbar';
+import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
-import {Editor, EditorState} from 'draft-js';
-
+import 'draft-js/dist/Draft.css';
+import './TextEditor.css';
 import {useStyles} from './CreateDialog.styles';
 
 export default function CreateDialog({
   mode = 'post',
-  isOpen = false,
+  open = false,
+  setOpen,
   handleCreatePost,
 }) {
+  // MUI theme-based css styles
   const classes = useStyles();
+
+  // Form Data
   const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
   const [isVisibleToStudents, setIsVisisbleToStudents] = useState(true);
   const [isAnonymous, setIsAnonymous] = useState(false);
-  const [error, setError] = useState('');
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const editor = useRef(null);
 
+  const [error, setError] = useState('');
   const validatePost = () => {
     if (title && content) {
       handleCreatePost({
         title: title,
-        content: content,
+        content: JSON.stringify(convertToRaw(editorState.getCurrentContent())),
         visible_to_students: isVisibleToStudents,
         anonymous: isAnonymous,
       });
     };
   };
 
+  const handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return true;
+    }
+    return false;
+  };
+
   return (
     <Dialog
       isFullScreen
-      open={isOpen}
+      open={open}
       classes={{paper: classes.root}}
     >
       {error}
-      <Typography>
-        {mode === 'post' ? 'Create a new post' : 'Create a new comment'}
-      </Typography>
-      <Input value={title} onChange={(e) => setTitle(e.target.value)} placeholder={'Post Title'} />
-      <TextField value={content} onChange={(e) => setContent(e.target.value)}/>
-      <div className={classes.switchContainer}>
-        <p> Visibile to Students ? </p>
-        <Switch checked={isVisibleToStudents} onChange={() => setIsVisisbleToStudents(!isVisibleToStudents)}/>
-      </div>
-      <div className={classes.switchContainer}>
-        <p>Anonymous ? </p>
-        <Switch checked={isAnonymous} onChange={() => setIsAnonymous(!isAnonymous)}/>
-      </div>
-      <Button onClick={validatePost}> Post </Button>
+      <DialogTitle className={classes.titleContainer}>
+        <Box display="flex" alignItems="center">
+          <Box flexGrow={1} >{mode === 'post' ? 'Create A New Post' : 'Create A New Comment'}</Box>
+          <Box>
+            <IconButton onClick={(e) => setOpen(false)}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </DialogTitle>
+      <DialogContent sx={{padding: 0}}>
+        <Input inputProps={{className: classes.inputTitle}} fullWidth
+          value={title} onChange={(e) => setTitle(e.target.value)} placeholder={'Put Title Here'} />
+        <div className={classes.toolbarContainer}>
+          <EditorToolbar editorState={editorState} setEditorState={setEditorState}/>
+        </div>
+        <Editor
+          ref={editor}
+          handleKeyCommand={handleKeyCommand}
+          editorState={editorState}
+          onChange={(editorState) => {
+            setEditorState(editorState);
+          }}
+        />
+        <Box display="flex" alignItems="center" justifyContent="flex-start" gap="20px" padding="10px">
+          <div className={classes.switchContainer}>
+            <p> Visibile to Students?</p>
+            <Switch checked={isVisibleToStudents} onChange={() => setIsVisisbleToStudents(!isVisibleToStudents)}/>
+          </div>
+          <div className={classes.switchContainer}>
+            <p>Anonymous?</p>
+            <Switch checked={isAnonymous} onChange={() => setIsAnonymous(!isAnonymous)}/>
+          </div>
+        </Box>
+      </DialogContent>
+      <Button className={classes.submit} onClick={validatePost}> Post </Button>
     </Dialog>
   );
 }
--- web/src/components/forums/CreateDialog/CreateDialog.styles.jsx
+++ web/src/components/forums/CreateDialog/CreateDialog.styles.jsx
@@ -1,12 +1,39 @@
 import makeStyles from '@mui/styles/makeStyles';
 
-export const useStyles = makeStyles(() => ({
+export const useStyles = makeStyles((theme) => ({
   root: {
     width: '800px',
   },
   switchContainer: {
     display: 'flex',
     alignItems: 'center',
+  },
+  editorContainer: {
+    display: 'block',
+    height: '400px',
+    margin: theme.spacing(10),
+    padding: theme.spacing(1),
+  },
+  buttonIcon: {
+    color: theme.palette.primary.main,
+  },
+  toolbarContainer: {
+    borderColor: theme.palette.white,
+    borderTop: theme.spacing(0.1) + ' solid',
+    borderBottom: theme.spacing(0.1) + ' solid',
+  },
+  submit: {
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  },
+  titleContainer: {
+    backgroundColor: `${theme.palette.primary.main}80`,
+    padding: theme.spacing(1.5),
+  },
+  inputTitle: {
+    padding: theme.spacing(1.5, 1, 1.5, 1),
+    fontSize: '1rem',
+    borderBottom: '0', // Not working
   },
 }));
 
