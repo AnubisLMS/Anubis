cb2a3adef039d988d0b5fd8fc335537a503dd8f0
dolf321 <rami.r.richani02@hotmail.com>
2023-07-20 20:05:00 +0300

    CHG migrate text editor to new folder

--- web/src/components/forums/Editor/RichTextEditor.jsx
+++ web/src/components/forums/Editor/RichTextEditor.jsx
@@ -1,12 +1,16 @@
-import React, {useState, useRef} from 'react';
-import {EditorState, RichUtils, convertToRaw} from 'draft-js';
+import React, {useState, useRef, useEffect} from 'react';
+import {EditorState, RichUtils, convertFromRaw, convertToRaw} from 'draft-js';
 import Editor, {composeDecorators} from '@draft-js-plugins/editor';
 import createResizablePlugin from '@draft-js-plugins/resizeable';
 import EditorToolbar from './Toolbar/EditorToolbar';
 import createImagePlugin from '@draft-js-plugins/image';
 import {makeStyles} from '@mui/styles';
+import {Box, IconButton} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
 import 'draft-js/dist/Draft.css';
 import './TextEditor.css';
+
 const resizeablePlugin = createResizablePlugin();
 const decorator = composeDecorators(
   resizeablePlugin.decorator,
@@ -15,19 +19,34 @@ const imagePlugin = createImagePlugin({decorator});
 
 const useStyles = makeStyles((theme) => ({
   toolbarContainer: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     borderColor: theme.palette.white,
     borderTop: theme.spacing(0.1) + ' solid',
     borderBottom: theme.spacing(0.1) + ' solid',
   },
+  editorContainer: {
+    width: '100%',
+  },
 }));
 
-export default function RichTextEditor({getContent}) {
+export default function RichTextEditor({content = null, setContent = null, setOpen = null, readOnly = false,
+  enableToolbar = true}) {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const editor = useRef(null);
   const classes = useStyles();
-  getContent = () => {
-    return JSON.stringify(convertToRaw(editorState.getCurrentContent()));
-  };
+
+  if (!readOnly && setContent) {
+    useEffect(() => {
+      setContent(JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+    }, [editorState]);
+  } else if (content) {
+    useEffect(() => {
+      setEditorState(EditorState.createWithContent(convertFromRaw(content)));
+    }, []);
+  }
 
   const handleKeyCommand = (command) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -39,16 +58,26 @@ export default function RichTextEditor({getContent}) {
   };
   return (
     <>
-      <div className={classes.toolbarContainer}>
+      {enableToolbar &&
+      <Box className={classes.toolbarContainer}>
         <EditorToolbar editorState={editorState} setEditorState={setEditorState} imagePlugin={imagePlugin} />
-      </div>
+        {
+          setOpen &&
+          <IconButton onClick={() => setOpen(false)}>
+            <CloseIcon />
+          </IconButton>
+        }
+      </Box>
+      }
       <Editor
         ref={editor}
+        className={classes.editorContainer}
         handleKeyCommand={handleKeyCommand}
         editorState={editorState}
         onChange={(editorState) => {
           setEditorState(editorState);
         }}
+        readOnly={readOnly}
         plugins={[imagePlugin, resizeablePlugin]}
       />
     </>
