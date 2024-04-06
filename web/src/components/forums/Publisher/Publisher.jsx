2c333060a41fe4cba283a4d8bb5af47a048322fc
Joe <joe@osiris.cyber.nyu.edu>
2024-04-05 20:07:02 -0400

    CHG editor from RichText to Markdown

--- web/src/components/forums/Publisher/Publisher.jsx
+++ web/src/components/forums/Publisher/Publisher.jsx
@@ -44,12 +44,12 @@ export default function Publisher({
   const classes = useStyles();
   // Form Data
   const [title, setTitle] = useState(initalTitle);
-  const [content, setContent] = useState(initialContent ? initialContent : {});
+  const [content, setContent] = useState(initialContent);
   const [isVisibleToStudents, setIsVisisbleToStudents] = useState(visibleToStudents);
   const [isAnonymous, setIsAnonymous] = useState(anonymous);
   const {enqueueSnackbar} = useSnackbar();
 
-  const isPost = mode === 'post' || mode==='edit_post';
+  const isPost = mode === 'post' || mode === 'edit_post';
 
   const validatePost = () => {
     if (title && content) {
@@ -68,42 +68,60 @@ export default function Publisher({
       });
       if (onClose) onClose();
     } else {
-      enqueueSnackbar('Add content!', {variant: 'warning'});
+      enqueueSnackbar('Post title and body cannot be empty', {variant: 'warning'});
     }
   };
 
   return (
     <Box className={classes.publisherContainer}>
       {isPost &&
-        <DialogTitle className={classes.titleContainer}>
-          <Box display="flex" alignItems="center">
-            <Box flexGrow={1}>{modes[mode].title}</Box>
-            <IconButton onClick={() => setOpen(false)}>
-              <CloseIcon/>
-            </IconButton>
-          </Box>
-        </DialogTitle>
+        <diV>
+          <DialogTitle className={classes.titleContainer}>
+            <Box display="flex" alignItems="center">
+              <Box flexGrow={1}> {modes[mode].title} </Box>
+              <IconButton onClick={() => setOpen(false)}>
+                <CloseIcon/>
+              </IconButton>
+            </Box>
+          </DialogTitle>
+          <Divider sx={{height: 1, backgroundColor: '#638efa'}} orientation="horizontal"/>
+        </diV>
       }
       <DialogContent sx={{padding: 0}}>
         {isPost &&
-          <Input inputProps={{className: classes.inputTitle}} disableUnderline={true} fullWidth
-            value={title} onChange={(e) => setTitle(e.target.value)} placeholder={modes[mode].title_placeholder}/>
+          <InputBase
+            sx={{pl: 1, paddingTop: 2, paddingBottom: 2, background: '#0d1117'}}
+            placeholder={modes[mode].title_placeholder}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+          />
         }
-        {isPost ?
-          <RichTextEditor setContent={setContent} content={initialContent} /> :
-          <RichTextEditor setContent={setContent} content={initialContent} setOpen={setOpen}/>
-        }
-        <Box display="flex" alignItems="center" justifyContent="flex-start" gap="20px" padding="0px 10px">
+        <div data-color-mode="dark">
+          <MDEditor value={content} onChange={setContent} minHeight={200}
+            previewOptions={{
+              rehypePlugins: [[rehypeSanitize]],
+            }}
+            textareaProps={{
+              placeholder: isPost ? 'Be specific and imagine you’re asking a question to another person. ' +
+                        'Describe what you tried and what you expected to happen.' : '',
+            }}
+          />
+        </div>
+        <Box display="flex" alignItems="center" justifyContent="flex-start" gap="20px" padding="0px 10px"
+          sx={{background: '#0d1117'}}
+        >
           <div className={classes.switchContainer}>
-            <Typography>Visible to Students?</Typography>
-            <Switch checked={isVisibleToStudents} onChange={() => setIsVisisbleToStudents(!isVisibleToStudents)}/>
+            <Typography variant={'caption'}>TA&apos;s and Instructors only</Typography>
+            <Switch checked={!isVisibleToStudents} onChange={() => setIsVisisbleToStudents(!isVisibleToStudents)}/>
           </div>
           <div className={classes.switchContainer}>
-            <Typography>Anonymous?</Typography>
+            <Typography variant={'caption'}>Anonymous</Typography>
             <Switch checked={isAnonymous} onChange={() => setIsAnonymous(!isAnonymous)}/>
           </div>
         </Box>
       </DialogContent>
+      <Divider sx={{height: 1, backgroundColor: '#638efa'}} orientation="horizontal"/>
       <Button
         variant={'contained'}
         className={classes.submit}
